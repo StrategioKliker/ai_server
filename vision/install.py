ccc9c431d6bc617b1e83d1f92924baa8c0db26f0
MarinLuic <marinluic@gmail.com>
2025-07-07 12:42:33 +0200

    seperate concerns

--- vision/install.py
+++ vision/install.py
@@ -2,8 +2,10 @@ import os
 import requests
 
 
-def init_projector_if_not_exists(self):
-    if not os.path.isfile(self._projector_path):
+def init_projector_if_not_exists():
+    projector_path = projector_path = os.path.join('minicpm', "mmproj-model-f16.gguf")
+    if not os.path.isfile(projector_path):
+    
         print("Downloading correct projector model...", flush=True)
         download_url = (
             "https://huggingface.co/openbmb/MiniCPM-V-2_6-gguf/resolve/"
@@ -13,11 +15,11 @@ def init_projector_if_not_exists(self):
         if resp.status_code != 200:
             raise RuntimeError("Failed to fetch projector GGUF file.")
 
-        with open(self._projector_path, "wb") as f:
+        with open(projector_path, "wb") as f:
             for chunk in resp.iter_content(chunk_size=8192):
                 if chunk:
                     f.write(chunk)
-        print("Projector downloaded:", self._projector_path, flush=True)
+        print("Projector downloaded:", projector_path, flush=True)
 
 
 if __name__ == '__main__':
