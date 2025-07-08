03fb28fa10e9ebf5a95c3ad85e85cdcb8552cff2
MarinLuic <marinluic@gmail.com>
2025-07-08 08:20:07 +0200

    update image handling for online images

--- vision/cpp.py
+++ vision/cpp.py
@@ -1,9 +1,13 @@
 import os
 import sys
+import imghdr
+import hashlib
 import requests
+import filetype 
 from datetime import datetime
-from typing import List, Union
 from llama_cpp import Llama
+from typing import List, Union
+from urllib.parse import urlparse, unquote
 from llama_cpp.llama_chat_format import MiniCPMv26ChatHandler
 
 
@@ -47,8 +51,10 @@ class ImageInference:
     def __init__(self):
         self.elapsed_minutes = 0
 
-        print("Loading MiniCPM model with multimodal support…", flush=True)
+        self.image_dir = 'images'
+        os.makedirs(self.image_dir, exist_ok=True)
 
+        print("Loading MiniCPM model with multimodal support…", flush=True)
         #  Ensure projector exists (correct GGUF file)
         projector_dir = "minicpm"
         self._projector_path = os.path.join(projector_dir, "mmproj-model-f16.gguf")
@@ -74,6 +80,78 @@ class ImageInference:
                 gpu_mlock=True  
             )
 
+
+    def __get_image_filename(self, img_url: str, img_content: bytes) -> Union[str, None]:
+        img_path = urlparse(img_url).path
+        img_url_base = unquote(img_path.rsplit('/', 1)[-1])
+        img_url_base = img_url_base or ''
+        img_url_base = img_url_base.rsplit('.', 1)[0]
+        if not img_url_base or len(img_url_base) < 3: 
+            img_url_base = f"img_{hashlib.sha256(img_content).hexdigest()[:12]}"
+
+
+        common_suffix = (".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tif", ".tiff", ".webp", ".svg", ".ico", ".heic", ".avif", ".jfif", ".apng" )
+        for suffix in common_suffix: 
+            if img_url.lower().endswith(suffix): 
+                suffix = suffix.lstrip('.')
+                return f"{img_url_base}.{suffix}"
+
+        img_kind = imghdr.what(None, img_content)
+        if img_kind:
+            return f"{img_url_base}.{img_kind}"
+        
+        img_info = filetype.guess(img_content)
+        if img_info: 
+            return f"{img_url_base}.{img_info.extension}"
+        
+        return None 
+
+    def __save_image(self, img_url: str) -> Union[str, None]:
+        res = requests.get(img_url, timeout=60)
+        if res is None or res.status_code != 200: 
+            print("Failed to get image on url: ", img_url, " with response: ", res.status_code)
+            return None 
+        
+        image_filename = self.__get_image_filename(img_url, res.content)   
+        if not image_filename: 
+            print("Failed to find image filename")
+            return None 
+        
+        local_path = os.path.join(self.image_dir, image_filename)
+        try: 
+            with open(local_path, 'wb') as f: 
+                f.write(res.content)
+            return f"file://{os.path.abspath(local_path)}"
+        except: 
+            return None 
+
+
+    def __process_image_content(self, images, content):
+        image_found = False 
+        for img in images:
+            image_path = None 
+
+            if os.path.isfile(img):
+                image_path = f"file://{os.path.abspath(img)}"    
+            elif img.startswith(("http://", "https://")):
+                image_path = self.__save_image()
+            
+            if image_path is None: 
+                print("Skipping unsupported image:", img, flush=True)
+                continue
+            
+            print("Reading image from path: ", image_path)
+            content.append({
+                "type": "image_url",
+                "image_url": {"url": image_path}
+            })
+            image_found = True 
+            
+    
+        if not image_found: 
+            return None         
+
+
     def prompt(self, prompt: str, system_prompt: Union[str, None], images: List[str]) -> Union[str, None]:
         print("Running prompt", flush=True)
         if not prompt or not isinstance(prompt, str) or not images:
@@ -83,31 +161,10 @@ class ImageInference:
         # Build structured multimodal content
         content = [{"type": "text", "text": "Respond in English. " + prompt}]
 
-        image_found = False 
-        for img in images:
-            if os.path.isfile(img):
-                content.append({
-                    "type": "image_url",
-                    "image_url": {"url": f"file://{os.path.abspath(img)}"}
-                })
-                image_found = True 
-            elif img.startswith(("http://", "https://")):
-                res = requests.get(img)
-                if res is None or res.status_code != 200:
-                    print("Failed to get image from url: ", img, " with status code: ", res.status_code)
-                    continue
-                
-                content.append({
-                    "type": "image_url",
-                    "image_url": {"bytes": res.content}
-                })
-                image_found = True 
-            else:
-                print("Skipping unsupported image:", img, flush=True)
-
-        if not image_found: 
+        found_images = self.__process_image_content(images=images, content=content)
+        if not found_images: 
             print("No valid images")
-            return None 
+            return None         
 
         # Run the model
         start = datetime.now()
