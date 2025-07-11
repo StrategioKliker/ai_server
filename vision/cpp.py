28be821ebc811db901113250a7493a1e3a936805
MarinLuic <marinluic@gmail.com>
2025-07-11 11:17:14 +0200

    tweak for precision

--- vision/cpp.py
+++ vision/cpp.py
@@ -92,7 +92,7 @@ class ImageInference:
                 chat_handler=chat_handler,
                 n_threads=os.cpu_count(),
                 n_ctx=4096, 
-                n_gpu_layers=12,
+                n_gpu_layers=8,
                 main_gpu=0,     
                 gpu_mlock=False,
 
